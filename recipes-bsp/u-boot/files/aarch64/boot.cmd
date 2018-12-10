1048ece2f74f2c332f82928b9e32e09944ce4191
Roman Beránek <roman.beranek@prusa3d.com>
2018-12-10 15:51:57 +0100

    u-boot: removed drm.debug from cmdline

--- recipes-bsp/u-boot/files/aarch64/boot.cmd
+++ recipes-bsp/u-boot/files/aarch64/boot.cmd
@@ -5,7 +5,7 @@ if itest.b *0x10028 == 0x02 ; then
 	echo "U-boot loaded from eMMC or secondary SD"
 	rootdev=mmcblk2p2
 fi
-setenv bootargs console=${console} console=tty1 root=/dev/${rootdev} rootwait panic=10 drm.debug=0xf video=HDMI-A-1:D drm.edid_firmware=HDMI-A-1:edid/ls055r1sx04_148.5mhz.bin ${extra}
+setenv bootargs console=${console} console=tty1 root=/dev/${rootdev} rootwait panic=10 video=HDMI-A-1:D drm.edid_firmware=HDMI-A-1:edid/ls055r1sx04_148.5mhz.bin ${extra}
 load mmc 0:1 ${fdt_addr_r} ${fdtfile}
 load mmc 0:1 ${kernel_addr_r} Image
 booti ${kernel_addr_r} - ${fdt_addr_r}
