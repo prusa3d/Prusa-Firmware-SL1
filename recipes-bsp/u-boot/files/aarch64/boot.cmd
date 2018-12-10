27cd9f1fb043a2d4c2e54db86d53746fee010c65
Roman Beránek <roman.beranek@prusa3d.com>
2018-12-10 09:39:11 +0100

    u-boot: fixed rootdev value for eMMC boot

--- recipes-bsp/u-boot/files/aarch64/boot.cmd
+++ recipes-bsp/u-boot/files/aarch64/boot.cmd
@@ -3,7 +3,7 @@ rootdev=mmcblk0p2
 if itest.b *0x28 == 0x02 ; then
 	# U-Boot loaded from eMMC or secondary SD so use it for rootfs too
 	echo "U-boot loaded from eMMC or secondary SD"
-	rootdev=mmcblk1p2
+	rootdev=mmcblk2p2
 fi
 setenv bootargs console=${console} console=tty1 root=/dev/${rootdev} rootwait panic=10 drm.debug=0xf video=HDMI-A-1:D drm.edid_firmware=HDMI-A-1:edid/ls055r1sx04_148.5mhz.bin ${extra}
 load mmc 0:1 ${fdt_addr_r} ${fdtfile}
