2ff0cadb981d7fdeaf944d82703367b502e58be3
Roman Beránek <roman.beranek@prusa3d.com>
2018-12-18 16:39:10 +0100

    u-boot: load kernel & dtb from the storage u-boot got loaded from

--- recipes-bsp/u-boot/files/aarch64/boot.cmd
+++ recipes-bsp/u-boot/files/aarch64/boot.cmd
@@ -1,8 +1,8 @@
 # Default to (primary) SD
 rootdev=mmcblk0p2
-if itest.b *0x10028 == 0x02 ; then
-	# U-Boot loaded from eMMC or secondary SD so use it for rootfs too
-	echo "U-boot loaded from eMMC or secondary SD"
+if itest.b *0x10028 == 0x02 || itest.b *0x10028 == 0x12; then
+	# U-Boot loaded from eMMC so use it for rootfs too
+	echo "U-boot loaded from eMMC"
 	rootdev=mmcblk2p2
 fi
 setenv bootargs console=${console} root=/dev/${rootdev} rootwait panic=10 video=HDMI-A-1:D drm.edid_firmware=HDMI-A-1:edid/ls055r1sx04_148.5mhz.bin ${extra} logo.nologo vt.global_cursor_default=0
@@ -13,6 +13,6 @@ then
 	ums 0 mmc ${mmc_bootdev}
 fi
 
-load mmc 0:1 ${fdt_addr_r} ${fdtfile}
-load mmc 0:1 ${kernel_addr_r} Image
+load mmc ${mmc_bootdev}:1 ${fdt_addr_r} ${fdtfile}
+load mmc ${mmc_bootdev}:1 ${kernel_addr_r} Image
 booti ${kernel_addr_r} - ${fdt_addr_r}
