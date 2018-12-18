9c5f2bc6ee8d5214d57b6072d23e7a39ebefd608
Roman Beránek <roman.beranek@prusa3d.com>
2018-12-18 22:22:20 +0100

    boot.scr: inverted condition to make OTG mass storage mode opt-out

--- recipes-bsp/u-boot/files/aarch64/boot.cmd
+++ recipes-bsp/u-boot/files/aarch64/boot.cmd
@@ -1,13 +1,13 @@
 # Default to (primary) SD
-rootdev=mmcblk0p2
+rootdev=mmcblk0p1
 if itest.b *0x10028 == 0x02 || itest.b *0x10028 == 0x12; then
 	# U-Boot loaded from eMMC so use it for rootfs too
 	echo "U-boot loaded from eMMC"
-	rootdev=mmcblk2p2
+	rootdev=mmcblk2p1
 fi
 setenv bootargs console=${console} root=/dev/${rootdev} rootwait panic=10 video=HDMI-A-1:D drm.edid_firmware=HDMI-A-1:edid/ls055r1sx04_148.5mhz.bin ${extra} logo.nologo vt.global_cursor_default=0
 
-if fatsize mmc ${mmc_bootdev}:1 update_over_otg
+if ! fatsize mmc ${mmc_bootdev}:1 disable_mass_storage
 then
 	echo "Update over OTG - exposing boot media as a mass storage device"
 	ums 0 mmc ${mmc_bootdev}
