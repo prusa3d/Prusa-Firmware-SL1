aa56aebe1549f6d0f93929f43b4149aa66fdf62a
Vladimir Matena <vladimir.matena@prusa3d.cz>
2018-12-27 14:45:13 +0100

    Fix mounting boot partiton on emmc
    This passes systemd.setenv specifing boot partition device from uboot to
    systemd. boot.mount unit uses passed value to mount boot partition.

--- recipes-bsp/u-boot/files/aarch64/boot.cmd
+++ recipes-bsp/u-boot/files/aarch64/boot.cmd
@@ -1,11 +1,13 @@
 # Default to (primary) SD
 rootdev=mmcblk0p2
+bootdev=mmcblk0p1
 if itest.b *0x10028 == 0x02 || itest.b *0x10028 == 0x12; then
 	# U-Boot loaded from eMMC so use it for rootfs too
 	echo "U-boot loaded from eMMC"
 	rootdev=mmcblk2p2
+	bootdev=mmcblk2p1
 fi
-setenv bootargs console=${console} root=/dev/${rootdev} rootwait panic=10 video=HDMI-A-1:D drm.edid_firmware=HDMI-A-1:edid/ls055r1sx04_148.5mhz.bin ${extra} logo.nologo vt.global_cursor_default=0
+setenv bootargs console=${console} root=/dev/${rootdev} rootwait panic=10 video=HDMI-A-1:D drm.edid_firmware=HDMI-A-1:edid/ls055r1sx04_148.5mhz.bin ${extra} logo.nologo vt.global_cursor_default=0 systemd.setenv=BOOT_DEV=/dev/${bootdev}
 
 echo "Update over OTG - exposing boot media as a mass storage device"
 ums 0 mmc ${mmc_bootdev}
