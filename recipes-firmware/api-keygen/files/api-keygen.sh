7eaaa6caa96a820655f36c57506bdcc295a7aa02
Vladimir Matena <vladimir.matena@prusa3d.cz>
2019-02-01 10:36:42 +0100

    api-keygen: omit visually simlar characters

--- recipes-firmware/api-keygen/files/api-keygen.sh
+++ recipes-firmware/api-keygen/files/api-keygen.sh
@@ -4,7 +4,12 @@ DIR=/etc/sl1fw
 REALM=prusa-sl1
 USER=maker
 
-KEY=$(/usr/bin/base64 /dev/random | /usr/bin/head -c 8)
+while true; do
+	KEY=$(/usr/bin/base64 /dev/random | /usr/bin/head -c 8)
+	if echo ${KEY} | grep --quiet -v -e O -e 0 -e l -e I; then
+		break;
+	fi;
+done
 
 # Store apikey plain
 echo ${KEY} > ${DIR}/api.key
