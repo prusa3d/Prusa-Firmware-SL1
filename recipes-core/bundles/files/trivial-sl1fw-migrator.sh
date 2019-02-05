9e5695b12524230981c5d6b52b11689501dd0dad
Roman Beranek <roman.beranek@prusa3d.com>
2019-02-05 15:19:26 +0100

    expand the list of migrated configuration files

--- recipes-core/bundles/files/trivial-sl1fw-migrator.sh
+++ recipes-core/bundles/files/trivial-sl1fw-migrator.sh
@@ -5,7 +5,11 @@ case "$1" in
                 # only rootfs needs to be handled
                 test "$RAUC_SLOT_CLASS" = "rootfs" || exit 0
 
-                cp -av /etc/sl1fw/* ${RAUC_SLOT_MOUNT_POINT}/etc/sl1fw/
+                cp -av /etc/sl1fw ${RAUC_SLOT_MOUNT_POINT}/etc/							|| true
+                cp -av /etc/ssh/ssh_host_{ecdsa,ed25519,rsa}_key{,.pub} ${RAUC_SLOT_MOUNT_POINT}/etc/ssh/	|| true
+                cp -av {,${RAUC_SLOT_MOUNT_POINT}}/etc/wpa_supplicant/wpa_supplicant-wlan0.secrets.json		|| true
+                cp -av {,${RAUC_SLOT_MOUNT_POINT}}/etc/.updated							|| true
+                cp -av {,${RAUC_SLOT_MOUNT_POINT}}/etc/hostapd.secrets.json					|| true
                 ;;
         *)
                 exit 1
