967435b2ee4c51396033a5bec38fd7f91fc0657a
Vladimir Matena <vladimir.matena@prusa3d.cz>
2019-02-08 16:04:52 +0100

    u-boot env update: fix scan_dev_for_fdt

--- recipes-core/bundles/files/bundle_hook.sh
+++ recipes-core/bundles/files/bundle_hook.sh
@@ -15,8 +15,9 @@ case "$1" in
 		
 		if [ "$RAUC_SLOT_CLASS" = "bootloader" ]; then
 			echo "Updating u-boot environment"
-			fw_setenv scan_dev_for_fdt 'for prefix in ${boot_prefixes}; do if test -e mmc ${mmc_bootdev}:${rootpart} ${prefix}${fdtfile}; then setenv boot_prefix ${prefix}; if load mmc ${mmc_bootdev}: {rootpart} ${fdt_addr_r} ${prefix}${fdtfile}; then echo LOAD_FDT_OK; else echo LOAD_FDT_FAIL; reset; fi; break; elif test -e mmc ${mmc_bootdev}:${rootpart} ${prefix}${basefdt}; then setenv boot_prefix ${prefix}; if load mmc ${mmc_bootdev}:${rootpart} ${fdt_addr_r} ${prefix}${basefdt}; then echo LOAD_FDT_OK; else echo LOAD_FDT_FAIL; reset; fi; break; fi; done'
-
+			
+			fw_setenv scan_dev_for_fdt 'for prefix in ${boot_prefixes}; do if test -e mmc ${mmc_bootdev}:${rootpart} ${prefix}${fdtfile}; then setenv boot_prefix ${prefix}; if load mmc ${mmc_bootdev}:${rootpart} ${fdt_addr_r} ${prefix}${fdtfile}; then echo LOAD_FDT_OK; else echo LOAD_FDT_FAIL; reset; fi; break; elif test -e mmc ${mmc_bootdev}:${rootpart} ${prefix}${basefdt}; then setenv boot_prefix ${prefix}; if load mmc ${mmc_bootdev}:${rootpart} ${fdt_addr_r} ${prefix}${basefdt}; then echo LOAD_FDT_OK; else echo LOAD_FDT_FAIL; reset; fi; break; fi; done'
+			
 			fw_setenv bootcmd 'run bootcmd_prusa'
 
 			fw_setenv bootcmd_prusa_emmc 'run update_active_rootfs_slot; run scan_dev_for_fdt; setenv bootargs console=${console} root=/dev/mmcblk2p${rootpart} rootwait panic=10 video=HDMI-A-1:D drm.edid_firmware=HDMI-A-1:edid/ls055r1sx04_148.5mhz.bin ${extra} logo.nologo vt.global_cursor_default=0 rauc.slot=${slot}; if load mmc 1:${rootpart} ${kernel_addr_r} ${boot_prefix}Image; then echo LOAD_KERNEL_OK; else echo LOAD_KERNEL_FAIL; reset; fi; booti ${kernel_addr_r} - ${fdt_addr_r};'
