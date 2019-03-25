785bb124df8b5164b8f6b8e2b3b3e43055049ffc
Vladimir Matena <vladimir.matena@prusa3d.cz>
2019-03-25 16:32:21 +0100

    api-keygen: ban slashes, allow to set custom key

--- recipes-firmware/api-keygen/files/api-keygen.sh
+++ recipes-firmware/api-keygen/files/api-keygen.sh
@@ -4,12 +4,21 @@ DIR=/etc/sl1fw
 REALM=prusa-sl1
 USER=maker
 
-while true; do
-	KEY=$(/usr/bin/base64 /dev/random | /usr/bin/head -c 8)
-	if echo ${KEY} | grep -q -v -e O -e 0 -e l -e I; then
-		break;
+if [ -n "${1}" ]; then
+	KEY=${1}
+	
+	if [ $(echo -n ${KEY} | wc -c) -lt 8 ]; then
+		exit -1;
 	fi;
-done
+	
+else
+	while true; do
+		KEY=$(/usr/bin/base64 /dev/random | /usr/bin/head -c 8)
+		if echo ${KEY} | grep -q -v -e O -e 0 -e l -e I -e \\\\ -e /; then
+			break;
+		fi;
+	done
+fi;
 
 # Store apikey plain
 echo ${KEY} > ${DIR}/api.key
@@ -20,6 +29,3 @@ echo -n ${KEY} > ${DIR}/slicer-upload-api.key
 # Generate htdigest access for ${USER} with apikey password
 SIGN=$(echo -n "${USER}:${REALM}:${KEY}" | md5sum | sed 's/ .*//')
 echo "${USER}:${REALM}:${SIGN}" > ${DIR}/htdigest.passwd
-
-
-
