1bc442fddbed8e728ff1c7fc40664644b6e9cb4d
Vladimir Matena <vladimir.matena@prusa3d.cz>
2020-01-22 10:59:46 +0100

    Disable most of the bundle update hook
    - etc is distributed with this image, no need to update it
    - configuration is supposed to be reset, no need to copy it

--- sources/meta-prusa/recipes-core/bundles/files/bundle_hook.sh
+++ sources/meta-prusa/recipes-core/bundles/files/bundle_hook.sh
@@ -24,18 +24,6 @@ slot-post-install)
 		mkdir -p ${RAUC_MOUNT_PREFIX}/etc
 		mount ${etc_dev} ${RAUC_MOUNT_PREFIX}/etc
 		/lib/systemd/systemd-growfs ${RAUC_MOUNT_PREFIX}/etc
-		rsync --archive --delete --verbose /etc/ ${RAUC_MOUNT_PREFIX}/etc/
-
-		rm -f ${RAUC_MOUNT_PREFIX}/etc/dnsmasq.conf
-		sed -i 's/#DNSStubListener=udp/DNSStubListener=no/' ${RAUC_MOUNT_PREFIX}/etc/systemd/resolved.conf
-		rm -f ${RAUC_MOUNT_PREFIX}/etc/systemd/system/sockets.target.wants/sshd.socket
-		ln -sf /dev/null ${RAUC_MOUNT_PREFIX}/etc/systemd/system/sshd.socket
-
-		# Now default is set in /usr/lib/systemd/journald.conf.d/max_use.conf
-		sed -i 's/SystemMaxUse=.*/#SystemMaxUse=/' ${RAUC_MOUNT_PREFIX}/etc/systemd/journald.conf
-
-		grep 'RouteMetric=10' ${RAUC_MOUNT_PREFIX}/etc/systemd/network/20-eth.network || echo -e '[DHCP]\nRouteMetric=10\n' >> ${RAUC_MOUNT_PREFIX}/etc/systemd/network/20-eth.network
-		grep 'RouteMetric-20' ${RAUC_MOUNT_PREFIX}/etc/systemd/network/10-wlan.network || echo -e '[DHCP]\nRouteMetric=20\n' >> ${RAUC_MOUNT_PREFIX}/etc/systemd/network/10-wlan.network
 
 		umount ${etc_dev}
 
